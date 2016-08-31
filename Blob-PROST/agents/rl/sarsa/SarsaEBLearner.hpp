77d8795803c468a7fc09d570875bc749516a06a6
Suraj Narayanan S <surajx@gmail.com>
2016-09-01 00:01:48 +1000

    Documentation of function in hpp

--- Blob-PROST/agents/rl/sarsa/SarsaEBLearner.hpp
+++ Blob-PROST/agents/rl/sarsa/SarsaEBLearner.hpp
@@ -23,10 +23,52 @@ private:
     double beta,sigma;
 
     /**
-     * Constructor declared as private to force the user to instantiate SarsaLearner
-     * informing the parameters to learning/execution.
+     * Constructor declared as private to force the user to instantiate
+     * SarsaEBLearner informing the parameters to learning/execution.
      */
     SarsaEBLearner();
+
+    /**
+    * The distance between current state-action pair and a centroid in feature
+    * space. 
+    * @param vector<long long>& sa_cur a concatenation of the feature vector 
+    *        actions, obtain simulator screen, RAM, etc.
+    * @param vector<long long>& centroid One centroid obtained from a clustering
+    *        algorithm. If the number of visited state-action pairs is small,
+    *        the centroids may simply be positions of the visited state-action
+    *        pairs in feature space.
+    */
+    void metric(vector<long long>& sa_cur, vector<long long>& centroid);
+
+    /**
+    * Measure of similarity between state-action pairs and a centroid. 
+    * Computed using a gaussian kernel centered on the centroid. The Measure
+    * takes values between 0 and 1.
+    * @param vector<long long>& sa_cur A concatenation of the feature vector 
+    *        and action.
+    * @param vector<long long>& centroid One centroid obtained from a clustering
+    *        algorithm. If the number of visited state-action pairs is small,
+    *        the centroids may simply be positions of the visited state-action
+    *        pairs in feature space.
+    */
+    void similarity_measure(vector<long long>& sa_cur, vector<long long>& centroid);
+
+    /**
+    * A generalized state-action visit count. Roughly represents the visit
+    * density at a point in feature space. 
+    * @param vector<long long>& sa_cur A concatenation of the feature vector 
+    *        and action.
+    */    
+    double pseudo_count(vector<long long>& sa_cur);
+
+    /**
+    * A bonus added to the external reward to incentivise exploration. Roughly
+    * a local information reward for visiting a state-action pair. 
+    * @param vector<long long>& features The feature used in the linear approx.
+    * of the Q-function.
+    * @param int action The action for which the bonus is calculated.    
+    */
+    double exploration_bonus(vector<long long>& features, int action);    
 
 public:
 
@@ -36,7 +78,7 @@ public:
     *   - beta : Exploration rate.
     *   - sigma: Generalization factor.
     */
-    SarsaEBLearner(ALEInterface& ale, Features *features, Parameters *param,int seed);
+    SarsaEBLearner(ALEInterface& ale, Features *features, Parameters *param, int seed);
 
     void learnPolicy(ALEInterface& ale, Features *features);
 
